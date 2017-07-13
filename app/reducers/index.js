e3f1a52159d11f5a440748df3e3246a9405a7902
Luis Paredes <lparedesl@gmail.com>
2017-07-13 14:20:59 -0400

    added saved articles reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import SearchValues from './search';
 import ArticlesReducer from './article';
+import SavedArticlesReducer from './saved_article';
 
 const rootReducer = combineReducers({
     search_values: SearchValues,
     articles: ArticlesReducer,
+    savedArticles: SavedArticlesReducer,
     form: formReducer
 });
 
